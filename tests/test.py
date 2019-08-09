c3ff3f50b6877f803c57dd7215349191215cafa0
Nelissen <Mats.Nelissen@ing.com>
2019-08-09 18:17:54 +0200

    .

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 '''
 Provides connection to ATAG One Thermostat REST API
 
@@ -9,27 +9,27 @@ from pytag.gateway import AtagDataStore
 '''
 import asyncio
 import pprint
+
+
 async def test():
     """Test connection with imported TESTDATA dict"""
     from pyatag.gateway import AtagDataStore
     import aiohttp
-    from .input import TESTDATA
+    #from .input import TESTDATA
     pretty = pprint.PrettyPrinter(indent=2)
     async with aiohttp.ClientSession() as session:
-        atag = AtagDataStore(host=None, # TESTDATA["_host"],
+        atag = AtagDataStore(host=None,  # TESTDATA["_host"],
                              port=TESTDATA["_port"],
-                             #mail=None, # test with mail == None
+                             # mail=None, # test with mail == None
                              mail=TESTDATA["_mail"],
-                             interface=None, # TESTDATA["_interface"],
+                             interface=None,  # TESTDATA["_interface"],
                              session=session)
-
         await atag.async_update()
         assert atag.paired
-        #print('sensordata:')
-        #print(await atag.async_set_atag(dhw_mode_temp=44))
+        # print('sensordata:')
+        # print(await atag.async_set_atag(dhw_mode_temp=44))
         pretty.pprint(atag.sensordata)
-        #print(await atag.async_set_atag(_target_mode="manual", _target_temp=13))
-
+        # print(await atag.async_set_atag(_target_mode="manual", _target_temp=13))
         return atag
 
 asyncio.get_event_loop().run_until_complete(test())
