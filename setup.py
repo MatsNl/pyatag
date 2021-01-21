f4427992ed9fe7f840fcd486d31b85561ef92791
MatsNl <37705266+MatsNl@users.noreply.github.com>
2021-01-21 15:40:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open("README.md").read()
 
 setup(
     name="pyatag",
-    version="0.3.5",
+    version="0.3.5.1",
     license="MIT",
     url="https://github.com/MatsNl/pyatag",
     author="Mats Nelissen",
