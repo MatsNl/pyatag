0edd9aef3aa460ac8c5c51cdcc8f6dd3f99d1b9d
MatsNl <37705266+MatsNl@users.noreply.github.com>
2021-03-08 20:31:35 +0100

    Update entities.py

--- pyatag/entities.py
+++ pyatag/entities.py
@@ -304,7 +304,7 @@ class DHW:
         if self.status:
             return self._report["dhw_temp_setp"].state
         return self._report["dhw_mode_temp"].state
-    
+
     @property
     def current_operation(self):
         """Return the current operating mode (Eco or Performance (Comfort)."""
